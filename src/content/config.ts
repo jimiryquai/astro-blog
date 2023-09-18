306cf291bea4082f27452665f1184581fc39dc41
jimiryquai <james@jamesryan.dev>
2023-09-18 20:31:38 +0100

    added post and postslist components

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+// 2. Define your collection(s)
+const postsCollection = defineCollection({
+   type: 'content',
+   schema: z.object({
+      title: z.string(),
+      author: z.string(),
+      categories: z.array(z.string()),
+      date: z.string().transform((str) => new Date(str)),
+      featured: z.boolean(),
+      image: z.string(),
+  })
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  posts: postsCollection,
+};
